b404e71464828b2ab4cb9640fb1ea78b37827480
Tashrif <117228564+Tashrif2001@users.noreply.github.com>
2023-11-13 15:58:58 +0600

    Update Cfg_palindrome.cpp

--- Cfg_palindrome.cpp
+++ Cfg_palindrome.cpp
@@ -1,63 +1,52 @@
-#include <bits/stdc++.h>
-    using namespace std;
-    
-    bool found = false;
-    string path;
-    void cfg(vector<string> &base, string output, string target, string currpath)
-    {
-        if (found)
-            return;
-        
-        if(output.size()>target.size()+1)
-        return;
+#include<bits/stdc++.h>
+using namespace std;
 
-        if (output.find('P') == string::npos && output != " ")
-        {
-            if(output.find('E')!=string::npos)
-            {
-                string temp = output.substr(0, output.find('E'));
-                temp+=output.substr(output.find('E')+1);
-                output=temp;
-            }
-            if (output == target)
-            {
-                found = true;
-                path = currpath;
-            }
-            return;
-        }
-        
-        for (string x : base)
-        {
-            if (output.find('P') != string::npos)
-            {
-                string temp = output.substr(0, output.find('P'));
-                temp += x;
-                temp += output.substr(output.find('P') + 1);
-                x = temp;
-            }
-            cfg(base, x, target, currpath+"->"+x);
-            if(found) return;
-        }
-    }
-    
-    int main()
+bool found=false;
+string path;
+
+void cfg(vector<string>&base, string target, string curr, string currPath)
+{
+    if(found)
+    return;
+
+    if(curr.size()>target.size()+1)
+    return;
+
+    if(curr.find('P')==string::npos && curr!=" ")
     {
-        vector<string> base = { "E", "0", "1", "0P0", "1P1" };
-        string s;
-        cin >> s;
-        cfg(base, " ", s, path);
-        found ? cout << "Yes" : cout << "no";
-        cout<<endl;
-        if(found)
+        if(curr==target)
         {
-            if(path.find('E')!=string::npos){
-            string temp = path.substr(0, path.find('E'));
-            temp+=path.substr(path.find('E')+1);
-            path=temp;
-            }
-            
-            cout<<"P"+path<<endl;
+            found=true;
+            path = currPath;
         }
-        return 0;
+        return;
     }
+
+    for(string x : base)
+    {
+        if(curr.find('P')!=string::npos)
+        {
+            string temp = curr.substr(0, curr.find('P'));
+            temp+=x;
+            temp+=curr.substr(curr.find('P')+1);
+            x = temp;
+        }
+        cfg(base, target, x, currPath+"->"+x);
+        if(found)
+        return;
+    }
+}
+
+int main()
+{
+    vector<string>base={"", "0", "1", "0P0", "1P1"};
+    string s;
+    cin>>s;
+    cfg(base, s, " ", path);
+
+    if(found)
+    cout<<"P"<<path<<endl;
+    else
+    cout<<"Rejected"<<endl;
+    return 0;
+}
